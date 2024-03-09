908fd1bdf98abd94f3e90664a9c7fbd280932449
Christian Torres <cristian0steven714@hotmail.com>
2024-03-09 13:07:26 -0500

    creacion de proyecto Entidad financiera

---
+++ quind/src/main/java/com/test/quind/QuindApplication.java
@@ -0,0 +1,14 @@
+package com.test.quind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuindApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
